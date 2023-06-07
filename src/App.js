6de87e4c421190cd01a5c81aa32b5c2ff28f1c1f
Zachary <ZackCipolletta@gmail.com>
2023-06-07 14:28:08 -0700

    add comments to clarify some things

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useRef } from 'react';
 import './App.css';
-import './index.css';
-import AppDragDropDemo from './AppDragDropDemo';
 
 const App = () => {
+
+  const dragItem = useRef();
+  const dragOverItem = useRef();
+  const [list, setList] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6']);
+
+  const dragStart = (e, position) => {
+    dragItem.current = position;
+    console.log(e.target.innerHTML);
+  };
+
+  const dragEnter = (e, position) => {
+    dragOverItem.current = position;
+    console.log(e.target.innerHTML);
+  };
+
+  const drop = (e) => {
+    const copyListItems = [...list];
+    const dragItemContent = copyListItems[dragItem.current];
+    copyListItems.splice(dragItem.current, 1); // delete the current item being dragged from the list. This way we do not end up 
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent); // insert the dragItemContent into the list at the position of the draggedOverItem, deleting 0 items.
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setList(copyListItems);
+  };
+
   return (
-    <div>
-      <AppDragDropDemo />
-    </div>
+    <>
+      {
+        list &&
+        list.map((item, index) => (
+          <div style={{ backgroundColor: 'lightblue', margin: '20px 25%', textAlign: 'center', fontSize: '40px' }}
+            onDragStart={(e) => dragStart(e, index)}
+            onDragEnter={(e) => dragEnter(e, index)}
+            onDragEnd={drop}
+            key={index}
+            draggable>
+            {item}
+          </div>
+        ))}
+    </>
   );
 };
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
 export default App;
